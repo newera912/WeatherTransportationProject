cb617def3080736639d2e7cd06b9519f511b277e
Baojian Zhou <bzhou6@albany.edu>
2017-07-25 16:44:39 -0400

    apdm01

--- data/trafficData/I90_TravelTime/readMonthlyData.py
+++ data/trafficData/I90_TravelTime/readMonthlyData.py
@@ -4,6 +4,8 @@ import sys
 import numpy as np
 from calendar import monthrange
 print sys.maxsize
+from sets import Set
+
 csv.field_size_limit(922337203)
 direc='West'
 dataFile=open('I90'+direc+'TMCLatLon.txt','r')
@@ -13,7 +15,7 @@ tmcID={}
 for i,line in enumerate(dataFile.readlines()):
     line=line.strip().split()
     tmcID[line[0]]=i  
-print tmcID
+print len(tmcID),sorted(tmcID)
 edgeList=[]
 for i in range(len(tmcID)-1):    
     edgeList.append(str(i)+" "+str(i+1))
@@ -25,8 +27,9 @@ edgeFile.close()
 
 
 year=2016
-months=[3,4,5,6,7,8,9]
-
+months=[3,4,5,6,7,8,9,10,11,12]
+tmcs=Set()
+ex_tmcs=Set()
 for mon in months:
     dateList=[]
     a = monthrange(year, mon)
@@ -38,49 +41,53 @@ for mon in months:
         count=0     
                     
             
-        with open('I-90 West Full State',"r") as F:
+        with open('I90 west.csv',"r") as F:
             for line in F.readlines()[1:]:
                 d=line.strip().split(",")
                 if d[2]==date:
-                    
+                    tmcs.add(d[0])
                     timeSlot=int(d[1])
                     value=int(float(d[3]))
                     #print value
                     if not tmcID.has_key(d[0]):
+                        ex_tmcs.add(d[0])
                         continue
                     data[tmcID[d[0]]][timeSlot]=value
                     count+=1
                 else: 
                     continue
         
-        if np.sum(data)>0:                
-            with open('./wRaw/'+date+".txt","w") as output:
-                for i in range(0,len(data)):
-                    count2=0
-                    for j in range(0,len(data[0])):
-                        
-                        f=0
-                        if j==0 and data[i][j]==0:
-                            f=j+1
-                            while(data[i][f]==0):
-                                if f+1==288:
-                                    break
-                                f+=1
-                            data[i][j]=data[i][f]
-                        elif data[i][j]==0:
-                            f=j-1
-                            while(data[i][f]==0):
-                                if f-1==-288:
-                                    break
-                                f-=1
-                            data[i][j]=data[i][f]
-                        else:
-                            a=0   
-                        count2+=1        
-                        output.write(str(data[i][j])+" ")
-                    output.write("\n")
-                    if count2!=288:
-                        print count2
-        print count,"/",77*288    
+#         if np.sum(data)>0:                
+#             with open('./TravelTimeToEast/'+date+".txt","w") as output:
+#                 for i in range(0,len(data)):
+#                     count2=0
+#                     for j in range(0,len(data[0])):
+#                         
+#                         f=0
+#                         if j==0 and data[i][j]==0:
+#                             f=j+1
+#                             while(data[i][f]==0):
+#                                 if f+1==288:
+#                                     break
+#                                 f+=1
+#                             data[i][j]=data[i][f]
+#                         elif data[i][j]==0:
+#                             f=j-1
+#                             while(data[i][f]==0):
+#                                 if f-1==-288:
+#                                     break
+#                                 f-=1
+#                             data[i][j]=data[i][f]
+#                         else:
+#                             a=0   
+#                         count2+=1        
+#                         output.write(str(data[i][j])+" ")
+#                     output.write("\n")
+#                     if count2!=288:
+#                         print count2
+#         print date,count,"/",77*288    
             #print d[0]
     #print len(tmcList),("".join(list(tmcList)))
+print len(tmcs),tmcs
+print len(ex_tmcs),ex_tmcs
+
